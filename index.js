05d1aacf57877e36f2fc857f4c82867839ffaabe
mayurrohokale <mayurrohokale12345@gmail.com>
2025-03-06 18:40:34 +0530

    Implemented Recaptcha

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 const user_name = process.env.EMAIL_USER;
 const user_pass = process.env.EMAIL_PASS;
 const port = process.env.PORT || "http://localhost:3000";
+const RECAPTCHA_SECRET_KEY = process.env.SECRET_KEY;
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
@@ -28,12 +30,26 @@ app.get("/", (req, res) => {
 });
 
 app.post("/contact", async (req, res) => {
-  const { name, email, subject, message } = req.body;
+  const { name, email, subject, message, recaptchaToken } = req.body;
+
+  console.log("Name:", name, "| Email:", email, "| Subject:", subject, "| Message:", message, "| reCAPTCHA Token:", recaptchaToken);
 
   try {
-    if (!name || !email || !subject || !message) {
+    if (!name || !email || !subject || !message || !recaptchaToken) {
+    
       return res.status(400).json({ message: "All fields are required" });
       
+    }
+    const googleResponse = await axios.post( "https://www.google.com/recaptcha/api/siteverify",null,{
+        params: {
+            secret: RECAPTCHA_SECRET_KEY,
+            response: recaptchaToken,
+        }
+    })
+    console.log("✅ reCAPTCHA Response:", googleResponse?.data);
+    if (googleResponse?.data?.score < 0.5) {
+       
+        return res.status(400).json({ message: "Invalid reCAPTCHA" });
     }
 
     const mailoptions = {
